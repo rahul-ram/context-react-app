54b981ae585e35e9491e2f79b094d8d3f4dbdd70
Rahul Ram <rahul_ram7@live.com>
2018-08-14 00:26:20 +0530

    deploying to github pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
 import Contacts from "./components/contacts/Contacts";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Provider } from "./Context";
